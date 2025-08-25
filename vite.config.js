8cd078c05aff1233a5c349278c4398ffbd343644
Herick <herickmadruga8@gmail.com>
2025-08-25 05:06:16 -0300

    Configura o projeto para deploy com GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // --- ADICIONE ESTA LINHA ---
+  // Define a URL base da aplicação para o nome do seu repositório.
+  // Isso é CRUCIAL para que as imagens e outros arquivos carreguem corretamente.
+  base: "/TrinityCopy/", 
 })
